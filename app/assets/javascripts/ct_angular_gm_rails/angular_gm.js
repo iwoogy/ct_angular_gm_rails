2d3763ec4beb4c196359d4688fa9bcfbc7dc5039
kim <ksooksiri@codetalay.com>
2014-05-09 17:50:54 +0700

    commit message

---
+++ app/assets/javascripts/ct_angular_gm_rails/angular_gm.js
@@ -0,0 +1,4 @@
+//= require ./src/module
+//= require_tree ./src/services
+//= require_tree ./src/controllers
+//= require_tree ./src/directives
--- lib/ct_angular_gm_rails/version.rb
+++ lib/ct_angular_gm_rails/version.rb
@@ -1,3 +1,3 @@
 module CtAngularGmRails
-  VERSION = "0.0.1"
+  VERSION = "1.0.0"
 end
